6bcf7f7fdf201fd2cd7dc803547c3b4c4ac99be6
anka0501 <36705378+anka0501@users.noreply.github.com>
2024-07-06 16:51:50 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,7 +1,7 @@
 phone_number = 'XXXXXXXX'
 
-path = 'C:/Users/Guest/GoogleAppCredentials/bold-artifact-419614-3eac104b4907.json'
+path = 'C:/Users/GoogleAppCredentials/bold-artifact-419614-3eac104b4907.json'
 
-project_id = "bold-artifact-419614"  # @param {type:"string"}
+project_id = "your-project-name"  # @param {type:"string"}
 
 location = "us-central1"  # @param {type:"string"}
