b5eb253459763b9a676766e98108f957570f5beb
anka0501 <36705378+anka0501@users.noreply.github.com>
2024-07-06 17:02:51 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 phone_number = 'XXXXXXXX'
 
-path = 'C:/Users/GoogleKey/bold-artifact-419614-3eac104b4907.json'
+path = 'path/to/credentials/project-name-123456.json'
 
 project_id = "your-project-name"  # @param {type:"string"}
 
