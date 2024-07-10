aad53e016f05d0ef8dea27837a585d8a3ede744b
anka0501 <36705378+anka0501@users.noreply.github.com>
2024-07-10 16:45:17 +0200

    Update generate_message.py

--- generate_message.py
+++ generate_message.py
@@ -9,13 +9,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] =  path
 # initialize Vertex AI
 vertexai.init(project=project_id, location=location)
 
-
-def get_prompt(instruction, new_system_prompt):
-    prompt_template =  new_system_prompt + instruction
-    return prompt_template
-
-instruction = "User: {user_input}"
-system_prompt = """
+instruction = """
 Write a great pick-up line for a woman to use on a man, based on: {user_input}. 
 Don't write about yourself, focus on the guy. Keep it to one sentence.
 """
@@ -39,7 +33,7 @@ class MessageGemini:
 
       def model_gemini(self):
         prompt = PromptTemplate(
-                  input_variables=["user_input"], template=get_prompt(instruction, system_prompt)
+                  input_variables=["user_input"], template=instruction
                   )
         llm_chain = LLMChain(
                     llm=self.get_model(),
